5866ca06d0a863a2ee2fdaaf5af8e261df4fd7f6
Chinguun <Chinguunv@yahoo.com>
2021-08-11 22:54:34 +0800

    cloud function niig update hiisen

--- cloud-functions/secret-page.js
+++ cloud-functions/secret-page.js
@@ -1,6 +1,26 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: 'Heregchdiin huudsand tavtai morilno uu'
-    });
+    const secretContent = `
+    <h3>Hereglegchiin tusgai huudsand tavtai morilno uu!</h3>
+    <p>Zuvhun burtgeltei hereglegchded zoriulsan haalttai content.</p>
+    `
+    
+    let body;
+
+    if (event.body) {
+        body = JSON.parse(event.body);
+    } else {
+        body = {}
+    }
+
+    if (body.password == "JavaScript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent,
+        });
+    }
+    else {
+        callback(null, {
+            statusCode: 401,
+        });
+    }  
 };
